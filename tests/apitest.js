1e274b1d66db099e7fe8a222374b7cd07476f1a5
Madhuri Gandikota <madhuri.gandikota@accenture.com>
2024-10-16 21:55:16 +0530

    Added and committed apitest.js file

---
+++ tests/apitest.js
@@ -0,0 +1,16 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+const { request } = require('https');
+
+//Testing GET API
+test('GET /products/1', async ({ request }) => {
+    const response = await request.get('https://fakestoreapi.com/products/1');
+    const body = await response.json();
+  
+    //check response status is passed
+    expect(response.ok()).toBeTruthy(); 
+    // Check HTTP status code
+    expect(response.status()).toBe(200); 
+    // Console the response body
+    console.log(body); 
+  });
